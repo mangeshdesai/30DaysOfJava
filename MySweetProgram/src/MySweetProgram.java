cfa5305857d44b39c57a409a73ce63ff30c884d8
mangeshdesai <mangeshdesai@github.com>
2020-12-27 17:39:40 +0530

    convert queue to stack

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -1,21 +1,20 @@
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class MySweetProgram {
-    public static void main(String[] args) throws Exception {
-        String basePath = "C:\\Users\\mdesai\\workspace\\java\\30daysjava\\MySweetProgram\\src\\";
-        List<String> students = new ArrayList<>();
-        Scanner in = new Scanner(new File(basePath + "students.txt"));
 
-        while (in.hasNextLine()) {
-            students.add(in.nextLine());
-        }
+    public static void main(String[] args) {
+        // Stack - using LinkedList
 
-        for(String student : students) {
-            System.out.println(student);
-        }
-        in.close();
+        LinkedList<String> names = new LinkedList<String>();
+        names.push("Caleb");
+        names.push("Sue");
+        names.push("Sally");
+
+        // names.remove() - remove elements from front i.e. FIFO
+        // names.removeLast() - remove elements from end i.e. LIFO
+        System.out.println(names.remove());
+        System.out.println(names.remove());
+        System.out.println(names.remove());
     }
 }
