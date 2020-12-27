f211bde9394a317801838c4066c0a2dd1bb0aa00
mangeshdesai <mangeshdesai@github.com>
2020-12-27 17:04:40 +0530

    remove commented code

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -6,18 +6,8 @@ import java.util.Scanner;
 public class MySweetProgram {
     public static void main(String[] args) throws Exception {
         String basePath = "C:\\Users\\mdesai\\workspace\\java\\30daysjava\\MySweetProgram\\src\\";
-        String testFilename = basePath + "test";
-        String studentFilename = basePath + "students.txt";
-        Scanner in; // = new Scanner(new File(testFilename));
-
         List<String> students = new ArrayList<>();
-
-//        while(in.hasNextInt()) {
-//            System.out.println(in.nextInt());
-//        }
-//
-//        in.close();
-        in = new Scanner(new File(studentFilename));
+        Scanner in = new Scanner(new File(basePath + "students.txt"));
 
         while (in.hasNextLine()) {
             students.add(in.nextLine());
@@ -26,23 +16,6 @@ public class MySweetProgram {
         for(String student : students) {
             System.out.println(student);
         }
-
-
-//        System.out.println("Enter array size.");
-//        int size = in.nextInt();
-//
-//        int[] grades = new int[5];
-//
-//        System.out.println("Enter " + size + " numbers.");
-//
-//        for(int i = 0; i < 5; i++) {
-//            grades[i] = in.nextInt();
-//        }
-//
         in.close();
-//
-//        for(int i = 0; i < 5; i++) {
-//            System.out.println(grades[i]);
-//        }
     }
 }
