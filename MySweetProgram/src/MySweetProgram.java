994432d46d9daa2add5a31226230b9831003d5a9
mangeshdesai <mangeshdesai@github.com>
2020-12-27 18:27:22 +0530

    iterators

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -2,19 +2,23 @@ import java.io.File;
 import java.util.*;
 
 public class MySweetProgram {
-
     public static void main(String[] args) {
-        // Stack - using LinkedList
-
         LinkedList<String> names = new LinkedList<String>();
         names.push("Caleb");
         names.push("Sue");
         names.push("Sally");
 
-        // names.remove() - remove elements from front i.e. FIFO
-        // names.removeLast() - remove elements from end i.e. LIFO
-        System.out.println(names.remove());
-        System.out.println(names.remove());
-        System.out.println(names.remove());
+        ListIterator<String> it = names.listIterator();
+        it.next();
+        it.next();
+        it.add("Susan");
+
+//        while(it.hasNext()) {
+//            System.out.println(it.next());
+//        }
+
+        for(String s : names) {
+            System.out.println(s);
+        }
     }
 }
