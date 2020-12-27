1b0c0b6d7d1c32a7d69daddea7ac3708bbc3bbff
mangeshdesai <mangeshdesai@github.com>
2020-12-27 16:48:59 +0530

    add input from file

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -1,20 +1,48 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MySweetProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+        String basePath = "C:\\Users\\mdesai\\workspace\\java\\30daysjava\\MySweetProgram\\src\\";
+        String testFilename = basePath + "test";
+        String studentFilename = basePath + "students.txt";
+        Scanner in; // = new Scanner(new File(testFilename));
 
-        Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
+        List<String> students = new ArrayList<>();
 
-        if(s.equals("Caleb")) {
-            System.out.println("Hey Caleb");
-        } else if(s.equals("Sally")) {
-            System.out.println("Hey Sally");
-        } else {
-            System.out.println("You're not welcome here.");
+//        while(in.hasNextInt()) {
+//            System.out.println(in.nextInt());
+//        }
+//
+//        in.close();
+        in = new Scanner(new File(studentFilename));
+
+        while (in.hasNextLine()) {
+            students.add(in.nextLine());
         }
 
+        for(String student : students) {
+            System.out.println(student);
+        }
+
+
+//        System.out.println("Enter array size.");
+//        int size = in.nextInt();
+//
+//        int[] grades = new int[5];
+//
+//        System.out.println("Enter " + size + " numbers.");
+//
+//        for(int i = 0; i < 5; i++) {
+//            grades[i] = in.nextInt();
+//        }
+//
         in.close();
+//
+//        for(int i = 0; i < 5; i++) {
+//            System.out.println(grades[i]);
+//        }
     }
 }
